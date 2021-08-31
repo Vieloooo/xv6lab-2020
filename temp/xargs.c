6d0767f2c114a086c68e07d4134063eecfd6eb18
Vieloooo <arordavid@gmail.com>
2021-08-31 15:23:24 +0800

    modify xargs

--- temp/xargs.c
+++ temp/xargs.c
@@ -1,6 +1,12 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+/*  form of stdin 
+    there is no code for eof 
+    after read(buf,stdin)
+    there are just bytes in asc code 
+
+*/
 int main(int argc, char *argv[])
 {
     char buff[32] = {0};
@@ -17,13 +23,13 @@ int main(int argc, char *argv[])
         printf("no stdinput\n");
         exit(0);
     }
-    //put argv1-n in fake_argv 0-n-1
+    //put argv 1->n in fake_argv 0->n-1
     for (j = 1; j < argc; j++)
     {
         fake_argv[j - 1] = argv[j];
     }
 
-    //read buff, and copy block ( code between "\n") to fake
+    //read buff, and copy block ( code divided by "\n") to fake
     for (i = 0; i < buff_len; i++)
     {
         if (buff[i] == ' ')
@@ -32,7 +38,7 @@ int main(int argc, char *argv[])
             //question: how the proc adds the last arguement to fake_argv
             /*for examle: 
             echo hello|xargs echo gg
-                there is no' 'in the end of stdin,the stdin is [hello\0eof], 
+                there is no' 'in the end of stdin,the stdin is [hello,\0,eof], 
                 so how the proc recognize eof as ' ' ,and add the last arguement?
             */
             fake[k++] = 0;
