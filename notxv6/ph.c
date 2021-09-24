2e2af02da227b9706c8a476a2feff09f812cbd94
Vieloooo <arordavid@gmail.com>
2021-09-24 16:15:44 +0800

    add ph lock finish lab2

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,12 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t locks[NBUCKET];
+void initLocks(){
+  for (int i ;i<NBUCKET; i++){
+    pthread_mutex_init(&locks[i],NULL);
+  }
+}
 double
 now()
 {
@@ -24,7 +29,7 @@ now()
  gettimeofday(&tv, 0);
  return tv.tv_sec + tv.tv_usec / 1000000.0;
 }
-
+// insert a new k-v in the head of the bucket 
 static void 
 insert(int key, int value, struct entry **p, struct entry *n)
 {
@@ -42,10 +47,12 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  pthread_mutex_lock(&locks[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
   }
+  //
   if(e){
     // update the existing key.
     e->value = value;
@@ -53,6 +60,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -107,6 +115,7 @@ main(int argc, char *argv[])
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
   }
+  initLocks();
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
